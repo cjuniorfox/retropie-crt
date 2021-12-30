d00a2bc1307a79f5363250d8d775136c35bfb640
Carlos Junior <cjuniorfox@gmail.com>
2021-12-30 18:18:22 -0300

    Optimizations for Game Gear

--- resources/opt/retropie/configs/all/runcommand-onstart.sh
+++ resources/opt/retropie/configs/all/runcommand-onstart.sh
@@ -43,7 +43,7 @@ ntsc_gamegear(){
 	vcgencmd hdmi_timings 1392 0 41 124 124 240 0 2 2 17 0 0 4 58.920 0 25900000 1 && tvservice -e DMT\ 88 && tvservice -e DMT\ 87 && sleep 0.5 && fbset -xres 348 -yres 240 -depth 32
 }
 ntsc_megadrive(){
-	vcgencmd hdmi_timings 960 0 49 89 109 240 0 2 2 17 0 0 1 59.920 0 19000000 1 && tvservice -e DMT\ 88 && tvservice -e DMT\ 87 && sleep 0.5 && fbset -xres 960 -yres 240 -depth 32
+	vcgencmd hdmi_timings 1392 0 41 124 124 240 0 2 2 17 0 0 1 59.920 0 26400000 1 && tvservice -e DMT\ 88 && tvservice -e DMT\ 87 && sleep 0.5 && fbset -xres 1392 -yres 240 -depth 32
 }
 ntsc_mastersystem(){
 	vcgencmd hdmi_timings 1136 0 33 101 101 240 0 2 2 17 0 0 4 59.920 0 21500000 1 && tvservice -e DMT\ 88 && tvservice -e DMT\ 87 && sleep 0.5 && fbset -xres 284 -yres 240 -depth 32

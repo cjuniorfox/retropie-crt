bc372b9b0d2fdfa06059f4f67dfe81be509d234e
Carlos Junior <cjuniorfox@gmail.com>
2022-01-07 18:57:07 -0300

    test for ngpc

--- resources/usr/bin/consoledisp.py
+++ resources/usr/bin/consoledisp.py
@@ -68,7 +68,7 @@ def atari7800() :
     return getcmd(width=348,ntsc_freq=59.92)
 
 def ngp() :
-    return getcmd(width=160,ntsc_freq=60,oLeft=10,oRight=10)
+    return getcmd(width=160,ntsc_freq=60,oLeft=4,oRight=4)
 
 def n64() :
     return getcmd(width=696,ntsc_freq=60,oLeft=10,oRight=10)

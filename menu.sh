31b1e04252e7b8b5cf9bad256170518254ee34a4
Carlos Junior <cjuniorfox@gmail.com>
2022-01-20 23:08:40 -0300

    Initial setup and retropiemenu

--- menu.sh
+++ menu.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# check, if sudo is used
+if [[ "$(id -u)" -ne 0 ]]; then
+    echo "Script must be run under sudo from the user you want to install for. Try 'sudo $0'"
+    exit 1
+fi
+
 HEIGHT=15
 WIDTH=75
 CHOICE_HEIGHT=4
--- retropie-crt.sh
+++ retropie-crt.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git pull
-bash ./menu.sh
+sudo bash ./menu.sh

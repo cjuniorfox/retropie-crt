0e994e10620b440cdb173114c0436a1cfc34203f
Carlos Junior <cjuniorfox@gmail.com>
2022-01-20 22:52:58 -0300

    Initial setup and retropiemenu

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 cd /home/pi
-sudo apt update -y && sudo apt install git dialog -y
-git clone https://github.com/cjuniorfox/retropie-crt.git || cd retropie-crt && git pull && cd ..
+sudo apt-get update -y && sudo apt-get install git dialog -y
+git clone https://github.com/cjuniorfox/retropie-crt.git
 cd retropie-crt
+git pull
 cp resources/home/pi/RetroPie/retropiemenu/retropie-crt.sh /home/pi/RetroPie/retropiemenu/retropie-crt.sh
 sudo reboot

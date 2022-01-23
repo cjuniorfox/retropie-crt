8dda2c810bf9d78405a0488495e9ae195ecd9d2e
Carlos Junior <cjuniorfox@gmail.com>
2022-01-23 10:54:56 -0300

    runcommand now uses consoledisp

---
+++ resources/opt/retropie/configs/all/runcommand-onend-pal.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+consoledisp emulationstation --pal
--- resources/opt/retropie/configs/all/runcommand-onend.sh
+++ resources/opt/retropie/configs/all/runcommand-onend.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-timing=$(cat ../../../../boot/config.txt | grep hdmi_timings | sed 's/ngs=/ngs /g')
-vcgencmd "${timing}" && \
-	tvservice -e DMT\ 88 && tvservice -e DMT\ 87 && sleep 0.5 && \
-	fbset -depth 32 2>&1> /dev/null
+consoledisp emulationstation

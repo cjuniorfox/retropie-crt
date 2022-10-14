4632e8e6aee01b6ce9efab968d48a444dbb8342d
= <cjuniorfox@gmail.com>
2022-10-14 12:02:41 -0300

    Setup screen at the setup script installation

--- setup.sh
+++ setup.sh
@@ -5,4 +5,5 @@ git clone https://github.com/cjuniorfox/retropie-crt.git || echo "Repository alr
 cd retropie-crt
 git pull
 cp resources/home/pi/RetroPie/retropiemenu/retropie-crt.sh /home/pi/RetroPie/retropiemenu/retropie-crt.sh
-sudo ./menu.sh
+chmod +x /home/pi/RetroPie/retropiemenu/retropie-crt.sh
+echo "The initial setup process has been done. At the Pi itself, restart the EmulationStation and access the Retropie-crt option at the Retropie menu."

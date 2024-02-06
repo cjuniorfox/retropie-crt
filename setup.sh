2dc7014b32125330fc83007ec570985db138977b
Carlos Junior <cjuniorfox@gmail.com>
2024-02-06 18:56:42 -0300

    Update setup.sh
    Creating a directory for updating the setup.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 cd $HOME
 sudo apt-get update -y && sudo apt-get install git dialog -y
-git clone https://github.com/cjuniorfox/retropie-crt.git || echo "Repository already exists or could not had been cloned."
+git clone https://github.com/cjuniorfox/retropie-crt.git || echo "Repository already exists or could not have been cloned."
 cd retropie-crt
 git pull
-cp resources/home/pi/RetroPie/retropiemenu/retropie-crt.sh /home/pi/RetroPie/retropiemenu/retropie-crt.sh
-chmod +x /home/pi/RetroPie/retropiemenu/retropie-crt.sh
+mkdir -p $HOME/RetroPie/retropiemenu/
+cp resources/home/pi/RetroPie/retropiemenu/retropie-crt.sh $HOME/RetroPie/retropiemenu/retropie-crt.sh
+chmod +x $HOME/RetroPie/retropiemenu/retropie-crt.sh
 echo "The initial setup process has been done. At the Pi itself, restart the EmulationStation and access the Retropie-crt option at the Retropie menu."

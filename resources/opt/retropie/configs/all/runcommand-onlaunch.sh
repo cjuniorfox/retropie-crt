95c42adac14596441c0101277afbd44582ffbda9
Junior_FOX <cjuniorfox@gmail.com>
2024-02-09 18:39:43 -0300

    runcommand-onlaunch now using the chvideocore for mame-libretro

--- resources/opt/retropie/configs/all/runcommand-onlaunch.sh
+++ resources/opt/retropie/configs/all/runcommand-onlaunch.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-#tvservice -e "DMT 87"
-#sleep 3
-#fbset -depth 8
-#fbset -depth 32
-#tvservice -s
-
-#!/bin/bash
 #
 # Useful data are passed as arguments to these scripts:
 #
@@ -15,35 +8,12 @@
 # $4 - the full command line used to launch the emulator.
 #
 
-psx_dialog(){
-
-    OPTIONS=(1 "Standard Progressive (240p)"
-             2 "Hi-Res Interlaced (480i)")
-    CHOICE=$(dialog \
-                    --title "PSX Launcher" \
-                    --menu "Choose the desired resolution" \
-                    15 75 2 \
-                    "${OPTIONS[@]}" \
-                    2>&1 >/dev/tty)
-    clear
-    case $CHOICE in
-            1)
-                consoledisp psx 2>&1
-                ;;
-            2)
-                consoledisp psx_i 2>&1
-                ;;
-    esac
-}
-
 
 systemName="$1"
 emuName="$2"
-consoledisp "${systemName}" 2>&1> /dev/null
-#
-#if [[ "${systemName}" = "psx" ]]; then
-#    psx_dialog
-#else
-#    consoledisp "${systemName}" 2>&1> /dev/null
-#fi;
-#*
+command="$4"
+if [[ "${systemName}" == "mame-libretro" ]]; then
+        chvideocore "${command}"
+else    
+        consoledisp "${systemName}" 2>&1> /dev/null
+fi
